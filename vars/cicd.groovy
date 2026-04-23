e393df577103c464f95486071113d220134a0e4b
naveendonthula9963-prog <naveendonthula9963@gmail.com>
2026-04-23 15:37:00 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,8 +6,8 @@ def buildartifact()
 {
   sh 'mvn package'
 }
-def deployment(jodname,ip,context)
+def deployment(jobname,ip,context)
 {
-  sh "scp /home/ubuntu/myfile/workspace/${jodname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+  sh "scp /home/ubuntu/myfile/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 
