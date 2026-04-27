699361687ea4d4bcc8ecb08c885c3406e8e0d5fc
naveendonthula9963-prog <naveendonthula9963@gmail.com>
2026-04-27 16:51:51 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,4 +10,7 @@ def deployment(jobname,ip,context)
 {
 sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}"
 }
-
+def runselenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
