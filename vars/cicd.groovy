29267b46e7eebac2cf63ea93047ab87ee528b97e
naveendonthula9963-prog <naveendonthula9963@gmail.com>
2026-04-27 12:32:51 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,6 +8,6 @@ def buildartifact()
 }
 def deployment(jobname,ip,context)
 {
-  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}'
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}"
 }
 
